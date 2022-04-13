52a906c3ddb4533cb64d102231d7748f0c913940
Vitaliy <kutukov@list.ru>
2022-04-13 14:58:42 +0300

    next commit

--- main.cpp
+++ main.cpp
@@ -10,6 +10,24 @@
 
 #include <QDir>
 
+QImage BlurImage(QImage source)
+{
+    if(source.isNull()) return QImage();
+    QGraphicsScene scene;
+    QGraphicsPixmapItem item;
+    item.setPixmap(QPixmap::fromImage(source));
+
+    auto* blur = new QGraphicsBlurEffect;
+    blur->setBlurRadius(8);
+    item.setGraphicsEffect(blur);
+    scene.addItem(&item);
+    QImage result(source.size(), QImage::Format_ARGB32);
+    result.fill(Qt::transparent);
+    QPainter painter(&result);
+    scene.render(&painter, QRectF(), QRectF(0, 0, source.width(), source.height()));
+    return result;
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
